627ef659cf78dc46bc7617cbbd337ec1139c559a
mStrangers <mike.strangers@free.fr>
2020-10-02 11:19:00 +0200

    Add OSD AND more Sanity Check

---
+++ src/main/cms/cms_menu_altlim.c
@@ -0,0 +1,69 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "cli/settings.h"
+#include "flight/mixer.h"
+
+#include "cms/cms.h"
+#include "cms/cms_types.h"
+#include "cms/cms_menu_altlim.h"
+
+#include "config/feature.h"
+
+#include "config/config.h"
+
+
+
+
+
+static uint16_t cmsx_alt_cutoff_lim; // Cutt off altitude
+static uint16_t cmsx_alt_buffer_lim; // Buffer size
+
+static const void *cmsx_Altlim_OnEnter(displayPort_t *pDisp)
+{
+    UNUSED(pDisp);
+
+    cmsx_alt_cutoff_lim = mixerConfig()->alt_cutoff_lim;
+    cmsx_alt_buffer_lim = mixerConfig()->alt_buffer_lim
+
+    return NULL;
+}
+
+static const void *cmsx_Altlim_OnExit(displayPort_t *pDisp, const OSD_Entry *self)
+{
+    UNUSED(pDisp);
+    UNUSED(self);
+
+    ledStripConfigMutable()->ledstrip_race_color = cmsx_ledRaceColor;
+    ledStripConfigMutable()->ledstrip_beacon_color = cmsx_ledBeaconColor;
+    ledStripConfigMutable()->ledstrip_beacon_period_ms = cmsx_ledBeaconPeriod;
+    ledStripConfigMutable()->ledstrip_beacon_percent = cmsx_ledBeaconOnPercent;
+    ledStripConfigMutable()->ledstrip_beacon_armed_only = cmsx_ledBeaconArmedOnly;
+    ledStripConfigMutable()->ledstrip_visual_beeper = cmsx_ledVisualBeeper;
+    ledStripConfigMutable()->ledstrip_visual_beeper_color = cmsx_ledVisualBeeperColor;
+
+    return NULL;
+}
+
+static const OSD_Entry cmsx_menuAltlimEntries[] =
+{
+    { "-- ALT LIMIT --",  OME_Label, NULL, NULL, 0 },
+    { "CUTOFF",    OME_UINT16,NULL, &(OSD_UINT16_t){ &cmsx_alt_cutoff_lim, 0, 1000, 1 }, 0 },
+    { "BUFFER",    OME_UINT16,NULL, &(OSD_UINT16_t){ &cmsx_alt_buffer_lim, 0, 1000, 1 }, 0 },
+    { "BACK", OME_Back, NULL, NULL, 0 },
+    { NULL, OME_END, NULL, NULL, 0 }
+};
+
+CMS_Menu cmsx_menuLedstrip = {
+#ifdef CMS_MENU_DEBUG
+    .GUARD_text = "MENUALT",
+    .GUARD_type = OME_MENU,
+#endif
+    .onEnter = cmsx_Altlim_OnEnter,
+    .onExit = cmsx_Altlim_OnExit,
+    .onDisplayUpdate = NULL,
+    .entries = cmsx_menuAltlimEntries
+};
+
